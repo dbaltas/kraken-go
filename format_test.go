5ea0585b5fd1ff78bbfaffecb0e39a7d11698383
Dimitris Baltas <dimitris.baltas@thebeat.co>
2017-09-26 23:27:25 +0300

    display 24 hour low and high

--- format_test.go
+++ format_test.go
@@ -11,12 +11,16 @@ func TestFormat(t *testing.T) {
 	var close = []string{"380", "384"}
 	var volume = []string{"101000", "102000"}
 	var volumeAveragePrice = []string{"379", "102383"}
+	var high = []string{"385", "387"}
+	var low = []string{"372", "370"}
 	pairTicker.Close = close
 	pairTicker.Volume = volume
 	pairTicker.VolumeAveragePrice = volumeAveragePrice
-	pairTicker.OpeningPrice = 382.34
+	pairTicker.High = high
+	pairTicker.Low = low
+
 	output := formatPairTickerInfo(pair, *pairTicker)
-	expected := "BCH/EUR      380.000   382.340   102000.000 10443.066M"
+	expected := "BCH/EUR      380.000   370.000   387.000   102000.000 10443.066M"
 	if output != expected {
 		t.Errorf(`%s\n%s\n`, output, expected)
 	}
--- kraken.go
+++ kraken.go
@@ -11,11 +11,14 @@ import (
 func formatPairTickerInfo(pair string, pairTicker krakenapi.PairTickerInfo) string {
 	lastPrice, _ := strconv.ParseFloat(pairTicker.Close[0], 64)
 	volume, _ := strconv.ParseFloat(pairTicker.Volume[1], 64)
+	low, _ := strconv.ParseFloat(pairTicker.Low[1], 64)
+	high, _ := strconv.ParseFloat(pairTicker.High[1], 64)
 	volumeAveragePrice, _ := strconv.ParseFloat(pairTicker.VolumeAveragePrice[1], 64)
-	return fmt.Sprintf("%-10s %9.3f %9.3f %12.3f %9.3fM",
+	return fmt.Sprintf("%-10s %9.3f %9.3f %9.3f %12.3f %9.3fM",
 		pair,
 		lastPrice,
-		pairTicker.OpeningPrice,
+		low,
+		high,
 		volume,
 		volume*volumeAveragePrice/1000000)
 }
@@ -33,8 +36,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%-10s %9s %9s %12s %10s\n",
-		"Pair", "Last", "24h ago", "24h Volume", "24h EUR")
+	fmt.Printf("%-10s %9s %9s %9s %12s %10s\n",
+		"Pair", "Last", "24h low", "24h high", "24h Volume", "24h EUR")
 
 	fmt.Println(formatPairTickerInfo("ETH/EUR", ticker.XETHZGBP))
 	fmt.Println(formatPairTickerInfo("BCH/EUR", ticker.BCHEUR))
