1b756b738c9723ddb3cab92c9a34c47eed42cc2d
Dimitris Baltas <dimitris.baltas@thebeat.co>
2017-09-26 22:56:42 +0300

    go fmt

--- format_test.go
+++ format_test.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"testing"
 	"github.com/beldur/kraken-go-api-client"
+	"testing"
 )
 
 func TestFormat(t *testing.T) {
 	pairTicker := new(krakenapi.PairTickerInfo)
 	pair := "BCH/EUR"
-	var close = []string{"380","384"}  
-	var volume = []string{"101000","102000"}  
-	var volumeAveragePrice = []string{"379","102383"}  
+	var close = []string{"380", "384"}
+	var volume = []string{"101000", "102000"}
+	var volumeAveragePrice = []string{"379", "102383"}
 	pairTicker.Close = close
 	pairTicker.Volume = volume
 	pairTicker.VolumeAveragePrice = volumeAveragePrice
 	pairTicker.OpeningPrice = 382.34
-	output := formatPairTickerInfo(pair, *pairTicker)	
-	if (output != "BCH/EUR      380.000   382.340   102000.000 10443.066M") {
+	output := formatPairTickerInfo(pair, *pairTicker)
+	if output != "BCH/EUR      380.000   382.340   102000.000 10443.066M" {
 		t.Errorf(`%s\n%s\n`, output,
 			"BCH/EUR      380.000     0.000   102000.000 10443.066M")
 	}
--- kraken.go
+++ kraken.go
@@ -9,15 +9,15 @@ import (
 )
 
 func formatPairTickerInfo(pair string, pairTicker krakenapi.PairTickerInfo) string {
-	lastPrice, _ := strconv.ParseFloat(pairTicker.Close[0], 64);
-	volume, _ := strconv.ParseFloat(pairTicker.Volume[1], 64);
-	volumeAveragePrice, _ := strconv.ParseFloat(pairTicker.VolumeAveragePrice[1], 64);
+	lastPrice, _ := strconv.ParseFloat(pairTicker.Close[0], 64)
+	volume, _ := strconv.ParseFloat(pairTicker.Volume[1], 64)
+	volumeAveragePrice, _ := strconv.ParseFloat(pairTicker.VolumeAveragePrice[1], 64)
 	return fmt.Sprintf("%-10s %9.3f %9.3f %12.3f %9.3fM",
 		pair,
 		lastPrice,
 		pairTicker.OpeningPrice,
 		volume,
-		volume * volumeAveragePrice/1000000)
+		volume*volumeAveragePrice/1000000)
 }
 
 func main() {
